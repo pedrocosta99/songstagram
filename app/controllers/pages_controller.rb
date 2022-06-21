fb6fdb737c2d774166f7caf710446f53893eb342
Pedro Henrique Costa <pedrocosta99@gmail.com>
2022-06-20 21:23:54 -0300

    pages routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: %i[home explore profile]
 
   def home
+  end
+
+  def explore
+  end
+
+  def profile
   end
 end
