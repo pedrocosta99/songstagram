dd17696d3d5da9d62c9ce2e757d546a2084d5232
Pedro Henrique Costa <pedrocosta99@gmail.com>
2022-06-23 08:08:24 -0300

    navbar dropup and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,7 @@ class PagesController < ApplicationController
 
   def profile
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get "home", to: "pages#home", as: "home"
   get "profile", to: "pages#profile", as: "profile"
+  get "about", to: "pages#about", as: "about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
